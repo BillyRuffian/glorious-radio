ba49232330c24d57a5de6b483faddbe8eefe6552
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2023-11-11 15:09:04 +0000

    controls

---
+++ app/javascript/controllers/audio_player_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="audio-player"
+export default class extends Controller {
+  static targets = [ "audioSource", "playHead" ]
+
+  connect() {
+    console.log("connected");
+  }
+
+  playPause() {
+    if(this.audioSourceTarget.paused)
+      this.audioSourceTarget.play()
+    else
+      this.audioSourceTarget.pause()
+  }
+
+  playPositionUpdate() {
+    this.playHeadTarget.value = Math.floor(this.audioSourceTarget.currentTime)
+  }
+
+  metadata() {
+    this.playHeadTarget.max = Math.floor(this.audioSourceTarget.duration)
+  }
+}
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,8 +1,6 @@
 class Episode < ApplicationRecord
   belongs_to :feed
 
-  # after_create_commit -> { broadcast_prepend_later_to "feed-episodes", partial: "feed/episode", locals: { episode: self }, target: "episodes" }
-
   after_create_commit { broadcast_prepend_later_to feed, partial: "feeds/episode", locals: { episode: self }, target: "feed-episodes" }
   after_destroy_commit { broadcast_remove_to feed, target: self }
   after_update_commit { broadcast_replace_later_to feed, partial: "feeds/episode", locals: { episode: self }, target: self }
