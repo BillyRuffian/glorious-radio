08b400cdbdc97581edcc384a46b69b039c6b7b4d
Nigel Brookes-Thomas <nigel@brookes-thomas.co.uk>
2023-11-11 16:41:48 +0000

    playhead moving and resetting

--- app/javascript/controllers/audio_player_controller.js
+++ app/javascript/controllers/audio_player_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="audio-player"
 export default class extends Controller {
-  static targets = [ "audioSource", "playHead" ]
+  static targets = [ "audioSource", "playHead", "duration", "position" ]
 
   connect() {
     console.log("connected");
@@ -17,13 +17,30 @@ export default class extends Controller {
 
   playPositionUpdate() {
     this.playHeadTarget.value = Math.floor(this.audioSourceTarget.currentTime)
+    this.positionTarget.textContent = this.timeFormat(this.audioSourceTarget.currentTime)
   }
 
   metadata() {
+    this.durationTarget.textContent = this.timeFormat(this.audioSourceTarget.duration)
     this.playHeadTarget.max = Math.floor(this.audioSourceTarget.duration)
+    this.playPositionUpdate()
   }
 
   seek() {
     this.audioSourceTarget.currentTime = this.playHeadTarget.value
+  }
+
+  playHeadTargetConnected(element) {
+    if(this.audioSourceTarget.readyState > 0) 
+      this.metadata()
+    else
+      this.playHeadTarget.value = 0
+  }
+
+  timeFormat(secs) {
+    const minutes = Math.floor(secs / 60);
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${minutes}:${returnedSeconds}`;
   }
 }
