dc9fda88fea65cfffecaedc472c1ff279e097d26
Nigel Brookes-Thomas <nigel@brookes-thomas.co.uk>
2023-11-14 22:54:08 +0000

    disclosure button

---
+++ app/javascript/controllers/animator_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="animator"
+export default class extends Controller {
+  static targets = [ "animate", "control" ]
+
+  connect() {
+  }
+
+  slideDown() {
+    const displayedHeight = this.animateTarget.clientHeight
+    const contentHeight = this.animateTarget.scrollHeight
+
+    console.log(this.controlTarget)
+
+    if(displayedHeight < contentHeight ) {
+      this.animateTarget.style.height = contentHeight + "px"
+      this.controlTarget.textContent = "less"
+    }
+    else {
+      this.animateTarget.style.height = "5rem"
+      this.controlTarget.textContent = "more"
+    }
+
+    this.animateTarget.classList.toggle("text-transparent")
+  }
+}
