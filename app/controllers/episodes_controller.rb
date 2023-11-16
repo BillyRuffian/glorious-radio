e9d28c41f94cefb771d08f76af88382b9c934e6b
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2023-11-16 13:26:30 +0000

    player presents via turbo frame

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -4,6 +4,7 @@ class EpisodesController < ApplicationController
   def show
     @episode = Episode.includes(:feed).find_by(id: params[:id], feed_id: params[:feed_id])
     @feed = @episode.feed
+    render partial: 'audio_player' if turbo_frame_request?
   end
 
 end
